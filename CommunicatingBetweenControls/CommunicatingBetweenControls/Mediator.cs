5aaf292a18a1abf4844b83b3949ae6faf320f4d0
Sean Newman <seangnewman@msn.com>
2018-09-01 06:59:23 -0700

    Completed course

---
+++ CommunicatingBetweenControls/CommunicatingBetweenControls/Mediator.cs
@@ -0,0 +1,36 @@
+﻿using CommunicatingBetweenControls.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicatingBetweenControls
+{
+    public sealed class Mediator
+    {
+        // static members 
+        private static readonly Mediator _Instance = new Mediator();
+        private Mediator() { }
+
+        public static Mediator GetInstance()
+        {
+            return _Instance;
+        }
+
+        // Instance functionality
+        public event EventHandler<JobChangedEventArgs> JobChanged;
+        public void OnJobChanged(object sender, Job job)
+        {
+            var jobChangedDelegate = JobChanged as EventHandler<JobChangedEventArgs>;
+
+            //This will always be the pattern
+            if(jobChangedDelegate != null)
+            {
+                jobChangedDelegate(sender, new JobChangedEventArgs { Job = job });
+            }
+
+
+        }
+    }
+}
